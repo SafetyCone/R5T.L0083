98c0cc1242e3691cf652184ce52f1c2e25ff7dd1
davidcoats <david@getsafetycone.com>
2024-01-19 12:46:35 -0800

    Initial commit.

---
+++ source/R5T.L0083/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.L0083
+{
+	/// <summary>
+	/// LibGit2Sharp library.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.L0083/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.L0083
+LibGit2Sharp library.
